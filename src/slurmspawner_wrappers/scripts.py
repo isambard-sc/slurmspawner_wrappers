6ad2cfe4adb6c3bd3678f09f83c731954bfcc39d
James C. Womack <J.C.Womack@bristol.ac.uk>
2024-06-17 14:04:42 +0100

    Format

--- src/slurmspawner_wrappers/scripts.py
+++ src/slurmspawner_wrappers/scripts.py
@@ -4,15 +4,19 @@ Functions that wrap Slurm commands to be executed by JupyterHub via batchspawner
 The wrapper functions are to be used as console script entry points to the package, i.e. they should
 not accept any arguments and return an integer exit code.
 """
+
 import sys
+
 
 def run_sbatch() -> int:
     print("ERROR: function run_sbatch not implemented", file=sys.stderr)
     return 1
 
+
 def run_squeue() -> int:
     print("ERROR: function run_squeue not implemented", file=sys.stderr)
     return 1
+
 
 def run_scancel() -> int:
     print("ERROR: function run_squeue not implemented", file=sys.stderr)
